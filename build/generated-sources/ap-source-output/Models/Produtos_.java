a39db91920fa1ba14e37e8e606af462a4334d734
André Felipe Melo de Oliveira <andref841@gmail.com>
2024-02-25 05:38:45 -0300

    Uploading app src

---
+++ build/generated-sources/ap-source-output/Models/Produtos_.java
@@ -0,0 +1,21 @@
+package Models;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-25T05:12:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Produtos.class)
+public class Produtos_ { 
+
+    public static volatile SingularAttribute<Produtos, Double> preco;
+    public static volatile SingularAttribute<Produtos, Integer> codigo;
+    public static volatile SingularAttribute<Produtos, Integer> vendaId;
+    public static volatile SingularAttribute<Produtos, Integer> dadosItemVendaid;
+    public static volatile SingularAttribute<Produtos, Integer> quantidadeDisponivel;
+    public static volatile SingularAttribute<Produtos, String> nome;
+    public static volatile SingularAttribute<Produtos, Integer> unidade;
+    public static volatile SingularAttribute<Produtos, Integer> id;
+    public static volatile SingularAttribute<Produtos, String> dataUltimaVenda;
+
+}
---
+++ build/generated-sources/ap-source-output/Models/Vendas_.java
@@ -0,0 +1,16 @@
+package Models;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-25T05:12:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Vendas.class)
+public class Vendas_ { 
+
+    public static volatile SingularAttribute<Vendas, String> dataVenda;
+    public static volatile SingularAttribute<Vendas, String> metodoPagamento;
+    public static volatile SingularAttribute<Vendas, Double> valorTotal;
+    public static volatile SingularAttribute<Vendas, Integer> id;
+
+}
